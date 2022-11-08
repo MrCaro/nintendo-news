89ef8f072e1d6199e3eb66401d356aece49e18e6
MrCaro <36524539+MrCaro@users.noreply.github.com>
2022-11-08 15:23:54 -0500

    feat: refactor api news

---
+++ src/api/nintendo.js
@@ -0,0 +1,27 @@
+const DOMAIN = process.env.REACT_APP_NINTENDO_API_DOMAIN
+
+const ENDPOINTS = {
+  news: 'ign?p=1',
+}
+
+async function get() {
+  const url = `${DOMAIN}${ENDPOINTS.news}`
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_NINTENDO_API_KEY,
+      'X-RapidAPI-Host': process.env.REACT_APP_NINTENDO_API_HOST,
+    },
+  }
+
+  try {
+    const response = await fetch(url, options)
+    const apiResponse = await response.json()
+    return await apiResponse
+  } catch (err) {
+    console.error('yooo', err)
+  }
+}
+
+const NintendoNews = { get }
+export default NintendoNews
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,46 +1,26 @@
 import { useEffect, useState } from 'react'
+import NintendoNews from '../../api/nintendo'
 
 function Cards() {
   const [apiResponse, setApiResponse] = useState('')
 
   useEffect(() => {
-    getNintendoNews()
-  })
-
-  const nintendoAPI = 'https://nintendo-news1.p.rapidapi.com/news/ign?p=1'
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_NINTENDO_API_KEY,
-      'X-RapidAPI-Host': 'nintendo-news1.p.rapidapi.com',
-    },
-  }
-
-  const getNintendoNews = async () => {
-    try {
-      const response = await fetch(nintendoAPI, options)
-      const jsonResponse = await response.json()
-      setApiResponse(jsonResponse)
-    } catch (err) {
-      console.error(err)
-    }
-  }
+    ;(async () => {
+      const news = await NintendoNews.get()
+      setApiResponse(news)
+    })()
+  }, [])
 
   return (
     <div className="container mx-auto px-4 pb-24">
       {!apiResponse ? (
         <p className="text-center text-gray-500">Loading news...</p>
       ) : (
-        <ul
-          role="list"
-          className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
-        >
-          {apiResponse.articles.map((article, index) => (
-            <li key={index} className="relative">
+        <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+          {apiResponse.articles.map((article, i) => (
+            <li key={`news-${i}`} className="relative">
               <div className="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-grape-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                 <img
-                  // src={`https://source.unsplash.com/random/?sig=${index}`}
                   src={article.imgSrc}
                   alt=""
                   className="pointer-events-none object-cover group-hover:opacity-75"
